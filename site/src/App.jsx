d112ab17433e8a01407469c6271dacf341ea784c
Logan Claw <loganclaw9000@example.com>
2026-03-07 17:02:51 -0800

    [agent:frontend] scaffold app structure

---
+++ site/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
